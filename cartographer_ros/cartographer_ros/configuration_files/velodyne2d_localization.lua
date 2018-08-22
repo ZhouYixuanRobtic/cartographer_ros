d8383457f56d5aef11978d0b175f82c83dae222c
ZhouYixuanRobtic <39017590+ZhouYixuanRobtic@users.noreply.github.com>
2018-08-22 17:16:54 +0800

    Update velodyne2d_localization.lua

--- cartographer_ros/cartographer_ros/configuration_files/velodyne2d_localization.lua
+++ cartographer_ros/cartographer_ros/configuration_files/velodyne2d_localization.lua
@@ -15,6 +15,23 @@
 include "demo_velodyne2d.lua"
 
 TRAJECTORY_BUILDER.pure_localization = true
-POSE_GRAPH.optimize_every_n_nodes = 20
+POSE_GRAPH.optimize_every_n_nodes = 1
+
+MAP_BUILDER.num_background_threads= 8
+POSE_GRAPH.global_sampling_ratio = 0.003
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.3
+POSE_GRAPH.constraint_builder.min_score = 0.65
+
+MAP_BUILDER.use_trajectory_builder_2d = true
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 35
+TRAJECTORY_BUILDER_2D.min_range = 0.05
+TRAJECTORY_BUILDER_2D.max_range =25.
+TRAJECTORY_BUILDER_2D.missing_data_ray_length =1.
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight =10.
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 1e-1
+POSE_GRAPH.optimization_problem.huber_scale = 1e2
+
 
 return options
